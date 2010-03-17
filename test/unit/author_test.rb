5b5d89b829fde8e3ffd0ca03a86aeda9caf98e94
Kathy Little <klittle@edaya.com>
2010-03-16 23:10:35 -0700

    3 tests working

--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -7,16 +7,37 @@ class AuthorTest < ActiveSupport::TestCase
   fixtures  :authors
   
   def setup
-    @author = Author.find(params[:id])
+    #@author = Author.find([:id])
+  end
+  
+  def test_dog
+    assert_equal 5, 5
   end
   
   
-  test "unique author" do
-    author = Author.new(:author_last_name   => authors(:Munson).author_last_name,
-                        :author_first_name  => "xxx",
+  def test_should_create_author
+    author = Author.new
+      
+    author.author_last_name  =    "Silverstein"
+    author.author_first_name  =   "Shel"
+    author.author_id          =   10
+    
+    assert_equal author.author_last_name, "Silverstein"
+    assert_equal author.author_first_name, "Shel"
+    assert_equal author.author_id, 10
+    
+    assert author.save
+    
+  end
+
+  def test_unique_author
+    # author = Author.new(:author_last_name   => authors(:Munson).author_last_name,
+    author = Author.new(:author_last_name   => "Munson",
+                        :author_first_name  => "Derek",
                         :author_id          => 1)
                         
-    assert !product.save
-    assert_equal "has already been taken", product.errors.on(:author_last_name)
+    assert !author.save
+    #assert_equal "has already been taken", author.errors.on(:author_last_name)
+    assert_equal "This id is already in use", author.errors.on(:author_id)
   end
 end
