2fae3523e013f3d3586f4025d29d052f40af70bf
Kathy Little <klittle@edaya.com>
2010-03-18 22:10:52 -0700

    Got sort working but it is doubling layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,16 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
-  end
-
+    @users = User.find(:all, :order => params[:order])
+      respond_to do |wants|
+        wants.html do
+          render 
+        end
+        wants.js do
+          render :layout => false
+        end
+      end
+   end
   # GET /users/1
   # GET /users/1.xml
   def show
